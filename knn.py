f88d4e102e65d500248251d06cdcb0187727d88e
Marcio Lima <marcio.lima.inacio@gmail.com>
2019-04-23 17:28:16 -0300

    Plot inicial

--- knn.py
+++ knn.py
@@ -1,94 +1,117 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.neighbors import DistanceMetric
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import ShuffleSplit, KFold, LeaveOneOut
 
-def seq_deletion(knn,X,y):
+
+def seq_deletion(knn, X, y):
     x_copy = X.tolist()
     y_copy = y.tolist()
     i = 0
-    for x_i,y_i in zip(X,y):
+    for x_i, y_i in zip(X, y):
         x_copy = x_copy[:i]+x_copy[i+1:]
         y_copy = y_copy[:i]+y_copy[i+1:]
-        knn.fit(x_copy,y_copy)
+        knn.fit(x_copy, y_copy)
         p = knn.predict([x_i])
         if p[0] != y_i:
             x_copy = x_copy[:i]+[x_i]+x_copy[i:]
             y_copy = y_copy[:i]+[y_i]+y_copy[i:]
             i += 1
-        return x_copy,y_copy
+        return x_copy, y_copy
 
-def seq_insertion(knn,X,y):
+
+def seq_insertion(knn, X, y):
     x_copy = X[:knn.n_neighbors].tolist()
     y_copy = y[:knn.n_neighbors].tolist()
-    for x_i,y_i in zip(X,y):
-        knn.fit(x_copy,y_copy)
+    for x_i, y_i in zip(X, y):
+        knn.fit(x_copy, y_copy)
         p = knn.predict([x_i])
         if p[0] != y_i:
             x_copy.append(x_i)
             y_copy.append(y_i)
-    return x_copy,y_copy
+    return x_copy, y_copy
 
-def evaluate(knn,v,selection=None):
+
+def evaluate(knn, v, selection=None):
     score = []
     for train_index, test_index in v:
         X_train, X_test = x[train_index], x[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
         if selection != None:
-            X_train, y_train = selection(knn,X_train,y_train)
+            X_train, y_train = selection(knn, X_train, y_train)
         knn.fit(X_train, y_train)
         score.append(knn.score(X_test, y_test))
 
     mean = np.mean(score)
     std = np.std(score)
-    return mean,std
+    return mean, std
 
-def run_knn(metrics,ks,selection=None):
+
+def run_knn(metrics, ks, selection=None):
+    results = dict(k=[], metric=[], method=[], mean=[], std=[])
     for k in ks:
         for metric in metrics:
+            results['k'].extend(3*[k])
+            results['metric'].extend(3*[str(metric)])
             knn = KNeighborsClassifier(n_neighbors=k, metric=metric)
-            
+
             rs = ShuffleSplit(n_splits=10)
-            score = evaluate(knn,rs.split(x),selection)
-            print('ShuffleSplit',k,metric,score)
+            score = evaluate(knn, rs.split(x), selection)
+            results['method'].append('ShuffleSplit')
+            results['mean'].append(score[0])
+            results['std'].append(score[1])
+            print('ShuffleSplit', k, metric, score)
 
             kf = KFold(n_splits=10)
-            score = evaluate(knn,kf.split(x),selection)
-            print('KFold',k,metric,score)
+            score = evaluate(knn, kf.split(x), selection)
+            results['method'].append('KFold')
+            results['mean'].append(score[0])
+            results['std'].append(score[1])
+            print('KFold', k, metric, score)
 
             loo = LeaveOneOut()
-            score = evaluate(knn,loo.split(x),selection)
-            print('LeaveOneOut',k,metric,score)
+            score = evaluate(knn, loo.split(x), selection)
+            results['method'].append('LeaveOneOut')
+            results['mean'].append(score[0])
+            results['std'].append(score[1])
+            print('LeaveOneOut', k, metric, score)
+    return pd.DataFrame.from_dict(results)
+
 
 iris = datasets.load_iris()
 
 x = iris.data
 y = iris.target
 
-def seuclidean_3(x,y):
-    r = np.sqrt(np.sum((x - y)**2 / 3))
-    return r
 
 '''
 Primeiro slide: variar k e variar amostragem
 '''
 metrics = ['euclidean']
-ks = [1,2,3,4,5]
-run_knn(metrics,ks)
+ks = [1, 2, 3, 4, 5]
+run_knn(metrics, ks)
 
 '''
 Segundo slide: para um dado k: variar medidas de distância
 e comparar com algoritmos iterativos
 '''
-metrics = ['euclidean', 'manhattan', 'canberra', 'braycurtis', seuclidean_3]
+metrics = ['euclidean', 'manhattan', 'canberra', 'braycurtis']
 ks = [3]
-run_knn(metrics,ks)
+results = run_knn(metrics, ks)
 
-metrics = ['euclidean']
-print('seq_deletion')
-run_knn(metrics,ks,seq_deletion)
+# metrics = ['euclidean']
+# print('seq_deletion')
+# results_deletion = run_knn(metrics, ks, seq_deletion)
 
-print('seq_insertion')
-run_knn(metrics,ks,seq_insertion)
+# print('seq_insertion')
+# results_insertion = run_knn(metrics, ks, seq_insertion)
+
+groups = results.groupby(by='method')
+print(groups.groups.keys())
+plt.errorbar(results.index, results['mean'], results['std'],
+             linestyle='None', marker='o')
+plt.show()
